66b50e3ba95a718ca37c62cf8cbf2dea5247f11b
nekoko3 <e.tanaka.ism@gmail.com>
2026-02-01 10:47:54 +0900

    feature: Add example.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Python_Test
+
+### デモ
+
+https://www.loom.com/share/cc5b31d889dd4272bf4c5c4010e00b5e
---
+++ example.py
@@ -0,0 +1,34 @@
+# No.1
+print("---------No.1--------------")
+print("hello world")
+
+
+# No.2
+print("---------No.2--------------")
+def greet():
+    print("こんにちは")
+
+greet()
+
+
+# No.3
+print("---------No.3--------------")
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+print_name("ねここ")
+
+# No.4
+print("---------No.4--------------")
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+# No.5
+print("---------No.5--------------")
+def add(a, b):
+    return a + b
+
+print(add(30, 22))
+
